cae8907b21d5304f7e7f46959e1f3bc8f570a35a
DharcKhY <rodrigo96_@hotmail.com>
2021-05-31 10:50:28 -0300

    Added class: Position

---
+++ src/application/ClassTester.java
@@ -0,0 +1,14 @@
+package application;
+
+import tabuleiro.Position;
+
+public class ClassTester {
+
+	public static void main(String[] args) {
+		
+		Position objPosition = new Position();
+		objPosition = new Position(3, 5);
+		System.out.println(objPosition);
+	}
+
+}
